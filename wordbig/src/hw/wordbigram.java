690c09ce4bbf61230a39bd293529c5f78d4b804b
jchen81 <36244001+jchen81@users.noreply.github.com>
2018-11-15 23:36:06 -0500

    update

--- wordbig/src/hw/wordbigram.java
+++ wordbig/src/hw/wordbigram.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Parse {
+public class wordbigram {
     public static void main(String[] args) {
         String book;
         String dict;
